7d2e208827bcfcbb9eed77256f4f7c7ed03ee51c
andremlsantos <andremlsantos@gmail.com>
2020-05-21 22:45:10 +0200

    Updated dependencies and removed api key

--- src/helpers/unsplash.js
+++ src/helpers/unsplash.js
@@ -4,10 +4,10 @@ global.fetch = fetch;
 
 const Unsplash = require("unsplash-js").default;
 const unsplash = new Unsplash({
-    accessKey: "qvpjQXjjQEjYwfoqn1DcPRWXyBzy6akr8D1ZKJAH5tc",
-    secret: "IYI3SAW87GJi-7PlA0TdProl-eLN3hh8qSmb5wOKlYA",
-    callbackUrl: "urn:ietf:wg:oauth:2.0:oob",
-    bearerToken: "x6oI0hpYjvl0NCMrTlUHN_5akAdjrFzM24PqqwFNdWA",
+    accessKey: "",
+    secret: "",
+    callbackUrl: "",
+    bearerToken: "",
 });
 
 module.exports = unsplash;
